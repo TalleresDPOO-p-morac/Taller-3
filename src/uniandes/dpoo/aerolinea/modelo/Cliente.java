b22771d3f004d148ba12515f9f4acc50459821a8
Paula Mora Cárdenas <111018998+p-morac@users.noreply.github.com>
2024-02-22 21:38:14 -0500

    Add files via upload

---
+++ src/uniandes/dpoo/aerolinea/modelo/Cliente.java
@@ -0,0 +1,14 @@
+package uniandes.dpoo.aerolinea.modelo;
+
+public abstract class Cliente {
+	private ArrayList<Tiquete> tiquetesSinUsar;
+	private ArrayList<Tiquete> tiquetesUsados;
+	public Cliente() {
+		
+	}
+	
+	public String getTipoCliente() {
+		return null
+	}
+
+}
---
+++ src/uniandes/dpoo/aerolinea/modelo/Vuelo.java
@@ -0,0 +1,11 @@
+package uniandes.dpoo.aerolinea.modelo;
+
+public class Vuelo {
+	
+	private String fecha;
+	
+	public void Vuelo(Ruta ruta, String fecha, Avion avion) {
+		
+	}
+
+}
